1234374dff51d5b08da009bc103020c2211eee43
Bouh <bouh.abdillahi@polymtl.ca>
2023-12-27 00:16:56 -0500

    all sim donw

--- dice_throw.py
+++ dice_throw.py
@@ -3,6 +3,7 @@ Author: Ilwad Issa
 Description: 
 This Python script simulates rolling two six-sided dice a specified number of times and calculates the sum of the dice outcomes. 
 It then counts the frequency of each possible sum and generates a bar graph using matplotlib to visualize the distribution of the sums obtained.
+A gaussian curv is deduced from the shape of the distribution that emerges from the sum of discrete binomial random variables, which is the core statement of the Central Limit Theorem.
 Date : 26/12/2023
 """
 
@@ -31,7 +32,8 @@ frequency = {i: results.count(i) for i in possible_sums}
 plt.bar(frequency.keys(), frequency.values(), color='skyblue')
 plt.xlabel('Sum of Dice')
 plt.ylabel('Frequency')
-plt.title(f'Sum of {num_dice} Dice Rolling Frequency for {num_rolls} Rolls')
+plt.title(f'Sum of {num_dice} Dice for {num_rolls} Rolls')
 plt.xticks(possible_sums)
 plt.grid(axis='y')
+plt.savefig(f"Sum_of_{num_dice}_dices_for_{num_rolls}_rolls.png")
 plt.show()
